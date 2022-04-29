a5db7dea10771dfa8e9a15e91cf9eff05e69353f
İlker Doğan <ilkerdogan1215@gmail.com>
2022-04-29 14:20:55 +0300

    Veri tabanı sınıfları oluşturuldu.

---
+++ EntityLayer/Concrete/Seller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Seller
+    {
+        public int SellerID { get; set; }
+        public string SellerName { get; set; }
+        public string SellerMail { get; set; }
+        public string SellerPassword { get; set; }
+    }
+}
