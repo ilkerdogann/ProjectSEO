58bcc174493edd5c18ca175b6ffb47d08073b038
İlker Doğan <ilkerdogan1215@gmail.com>
2022-05-04 20:59:27 +0300

    Ürün işlemleri gerçekleştirildi.

--- DataAccessLayer/EntityFramework/EfProductDal.cs
+++ DataAccessLayer/EntityFramework/EfProductDal.cs
@@ -1,9 +1,11 @@
 ﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.Concrete.Repositories;
 using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
